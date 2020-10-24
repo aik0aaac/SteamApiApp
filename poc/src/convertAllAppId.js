f7ebcd2aa7beaaffea18a0661904a46224b5c5b9
aik0aaac <aik0aaac@gmail.com>
2020-10-24 18:03:51 +0900

    lint設定変更、pocフォルダ内のコードを整形

--- poc/src/convertAllAppId.js
+++ poc/src/convertAllAppId.js
@@ -1,19 +1,22 @@
-"use strict";
-//ファイル操作モジュールの追加
-var fs = require('fs');
-const path = require("path");
+'use strict'
+// ファイル操作モジュールの追加
+const fs = require('fs')
+const path = require('path')
 
-const fileName = "../outputAllAppId.json";
+const fileName = '../outputAllAppId.json'
 
-const fileRawData = fs.readFileSync(path.resolve(__dirname, fileName));
-const data = JSON.parse(fileRawData);
+const fileRawData = fs.readFileSync(path.resolve(__dirname, fileName))
+const data = JSON.parse(fileRawData)
 
 const writeFileName = '../outputAppName.txt'
-fs.writeFileSync(path.resolve(__dirname, writeFileName), "");
-data.applist.apps.forEach(e => {
+fs.writeFileSync(path.resolve(__dirname, writeFileName), '')
+data.applist.apps.forEach((e) => {
   try {
-    fs.appendFileSync(path.resolve(__dirname, writeFileName), `${e.name}, ${e.appid}\n`);
+    fs.appendFileSync(
+      path.resolve(__dirname, writeFileName),
+      `${e.name}, ${e.appid}\n`
+    )
   } catch (e) {
-    console.log(e);
+    console.log(e)
   }
-});
+})
--- poc/src/getNews.js
+++ poc/src/getNews.js
@@ -1,35 +1,35 @@
-"use strict";
-const fs = require("fs");
-const path = require("path");
-const https = require("https");
+'use strict'
+const fs = require('fs')
+const path = require('path')
+const https = require('https')
 
-let result = '';
+let result = ''
 
 const req = https.request(
-  "https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=1256670",
+  'https://api.steampowered.com/ISteamNews/GetNewsForApp/v2/?appid=1256670',
   (res) => {
-    res.on("data", (chunk) => {
-      result += chunk;
-    });
-    res.on("end", () => {
-      const data = JSON.parse(result);
+    res.on('data', (chunk) => {
+      result += chunk
+    })
+    res.on('end', () => {
+      const data = JSON.parse(result)
 
-      const fileName = "../outputGetNews.json";
-      fs.writeFileSync(path.resolve(__dirname, fileName), "");
+      const fileName = '../outputGetNews.json'
+      fs.writeFileSync(path.resolve(__dirname, fileName), '')
       try {
-        fs.appendFileSync(path.resolve(__dirname, fileName), result);
+        fs.appendFileSync(path.resolve(__dirname, fileName), result)
       } catch (e) {
-        console.log(e);
+        console.log(e)
       }
 
-      console.log(data.appnews.newsitems);
-      console.log("¥nJSONデータは以上です。");
-    });
+      console.log(data.appnews.newsitems)
+      console.log('¥nJSONデータは以上です。')
+    })
   }
-);
+)
 
-req.on("error", (e) => {
-  console.error(`エラーが出ました： ${e.message}`);
-});
+req.on('error', (e) => {
+  console.error(`エラーが出ました： ${e.message}`)
+})
 
-req.end();
+req.end()
